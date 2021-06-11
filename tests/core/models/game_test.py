9c9d556d5f34e16f1799d8abe8fde838c97d5c62
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-06-11 14:51:22 +0200

    initiated unit tests

---
+++ tests/core/models/game_test.py
@@ -0,0 +1,17 @@
+from tests.core.persist.utils import new_game
+
+
+def test_serialize():
+    game = new_game()
+    ser_game = game.serialize()
+    assert isinstance(ser_game, dict)
+    assert ser_game['_id'] == game.uid
+    assert ser_game['name'] == game.name
+    assert ser_game['description'] == game.description
+    assert ser_game['rules'] == game.rules
+    assert ser_game['duration_min'] == game.duration_min
+    assert ser_game['number_of_players'] == game.number_of_players
+    assert ser_game['image'] == game.image
+    assert ser_game['game_type'] == game.game_type.value
+    assert ser_game['created_at'] == game.created_at
+    assert ser_game['updated_at'] == game.updated_at
---
+++ tests/core/persist/utils.py
@@ -0,0 +1,39 @@
+from typing import Optional, Tuple, Dict
+
+from core.models.game import Game, GameType
+from core.models.cocktail import Cocktail, CocktailType
+from tests.utils import random_string, random_number, one_of
+
+
+def new_game(name: Optional[str] = None,
+             description: Optional[str] = None,
+             rules: Optional[str] = None,
+             duration_min: Optional[int] = None,
+             number_of_players: Optional[Tuple[int, int]] = None,
+             image: Optional[str] = None,
+             game_type: Optional[GameType] = None) -> Game:
+    game = Game()
+    game.name = name or random_string()
+    game.description = description or random_string()
+    game.rules = rules or random_string()
+    game.duration_min = duration_min or random_number(10, 60)
+    game.number_of_players = number_of_players or (random_number(1, 5), random_number(6, 10))
+    game.image = image or random_string()
+    game.game_type = game_type or one_of(GameType)
+    return game
+
+
+def new_cocktail(name: Optional[str] = None,
+                 ingredients: Optional[Dict[str, str]] = None,
+                 description: Optional[str] = None,
+                 preparation_time_min: Optional[int] = None,
+                 image: Optional[str] = None,
+                 cocktail_type: Optional[CocktailType] = None) -> Cocktail:
+    cocktail = Cocktail()
+    cocktail.name = name or random_string()
+    cocktail.ingredients = ingredients or {random_string(5,10) : random_string(5,10)}
+    cocktail.description = description or random_string()
+    cocktail.preparation_time_min = preparation_time_min or random_number(10, 60)
+    cocktail.image = image or random_string()
+    cocktail.cocktail_type = cocktail_type or one_of(CocktailType)
+    return cocktail
