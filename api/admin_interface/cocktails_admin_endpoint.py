0cf741a8d71590693431b1145a979d9667c57f29
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-07-17 11:52:23 +0200

    feat(admin-interfacte) add basic form to add cocktails in the DB (ingredient field does not work)

--- api/admin_interface/cocktails_admin_endpoint.py
+++ api/admin_interface/cocktails_admin_endpoint.py
@@ -1,10 +1,52 @@
+import logging
+
 from flask import Blueprint, jsonify, redirect, render_template
-from wtforms import Form, SubmitField
+from flask_wtf import FlaskForm, Form
+from wtforms import Form, SubmitField, StringField, IntegerField, SelectField, FormField, FieldList
+from wtforms.validators import DataRequired
+from core.models.cocktail import CocktailType, COCKTAIL_NAMES_TYPES, Cocktail, COCKTAIL_TYPES_NAMES
+from core.persist import cocktails_repository
+
+logger = logging.getLogger(__name__)
 
 admin_cocktails_blueprint = Blueprint('admin/cocktails/', __name__, template_folder='client/')
 
 
-@admin_cocktails_blueprint.route('/', methods=['GET'])
+class IngredientForm(FlaskForm):
+    ingredient_name = StringField('Ingrédient')
+    quantity = StringField('Quantité et unité (ex: 5g)')
+
+
+class CoctkailForm(FlaskForm):
+    name = StringField('Nom du cocktail:', validators=[DataRequired()])
+    description = StringField('Description du cocktail:', validators=[DataRequired()])
+    ingredients = FieldList(FormField(IngredientForm), min_entries=1)
+    preparation_time_min = IntegerField('Estimation de la durée de préparation du cocktail (minutes):', validators=[DataRequired()])
+    image = StringField("Lien de l'image du cocktail")
+    cocktail_type = SelectField("Type du cocktail:", choices=COCKTAIL_TYPES_NAMES.values())
+    submit = SubmitField('Enregister')
+
+
+@admin_cocktails_blueprint.route('/', methods=['GET', 'POST'])
 def cocktails():
-    res = {'status': 'c niquel'}
-    return jsonify(res)
+    form = CoctkailForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        ingredients = {f.data['ingredient_name']: f.data['quantity'] for f in form.ingredients.data}
+        preparation_time_min = form.preparation_time_min.data
+        image = form.image.data
+        cocktail_type = form.cocktail_type.data
+        cocktail = Cocktail(name=name, description=description, ingredients=ingredients,
+                            preparation_time_min=preparation_time_min,
+                            image=image, cocktail_type=COCKTAIL_NAMES_TYPES[cocktail_type])
+
+        res = cocktails_repository.create_one(cocktail)
+        if str(res.inserted_id) != str(cocktail._id):
+            logger.warning(f'Error when inserting the cocktail'
+                           f'inserted_id : {res.inserted_id}, cocktail_id : {cocktail._id}')
+            return render_template('error.html', entity='cocktail')
+        return redirect('../../admin')
+
+    return render_template('cocktails.html', form=form, message=message)
