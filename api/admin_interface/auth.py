def297f74c2153de8c0efbc4bba534dc1e36f012
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-07-22 12:19:38 +0200

    fix(admin-interface) fixed argument error in authentification

--- api/admin_interface/auth.py
+++ api/admin_interface/auth.py
@@ -15,7 +15,7 @@ def auth_required(f: Callable):
         if auth and auth.username == config.flask_admin.auth_username \
                 and auth.password == config.flask_admin.auth_password:
             return f(*args, **kwargs)
-        logger.warning(f'Username expected: {config.flask_admin.auth_username}'
+        logger.warning(f'Username expected: {config.flask_admin.auth_username} \n'
                        f'Password expected: {config.flask_admin.auth_password}')
         return make_response("On a pas pu t'authentifier mon Daniel,"
                              " en plus t'as pas le passe sanitaire ça rentre même pas", 401,
