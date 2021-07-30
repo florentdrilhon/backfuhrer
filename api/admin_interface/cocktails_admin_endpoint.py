87d70b737a71c1f7132611a0fef8245675b2245c
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-07-30 10:59:22 +0200

    refactor(admin-cocktails): improved cocktail form visual + security

--- api/admin_interface/cocktails_admin_endpoint.py
+++ api/admin_interface/cocktails_admin_endpoint.py
@@ -3,8 +3,7 @@ import logging
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
 
-
-from wtforms import SubmitField, StringField, IntegerField, SelectField, FormField, FieldList
+from wtforms import SubmitField, StringField, IntegerField, SelectField, FormField, FieldList, TextAreaField
 from wtforms.validators import DataRequired
 from core.models.cocktail import COCKTAIL_NAMES_TYPES, Cocktail, COCKTAIL_TYPES_NAMES
 from core.persist import cocktails_repository
@@ -22,8 +21,8 @@ class IngredientForm(FlaskForm):
 
 class CocktailForm(FlaskForm):
     name = StringField('Nom du cocktail:', validators=[DataRequired()])
-    description = StringField('Description du cocktail:', validators=[DataRequired()])
-    ingredients = FieldList(FormField(IngredientForm), min_entries=8, max_entries=8)
+    description = TextAreaField('Description du cocktail:', validators=[DataRequired()])
+    ingredients = FieldList(FormField(IngredientForm), min_entries=6)
     preparation_time_min = IntegerField('Estimation de la durée de préparation du cocktail (minutes):',
                                         validators=[DataRequired()])
     image = StringField("Lien de l'image du cocktail")
@@ -38,21 +37,23 @@ def cocktails():
     message = ""
     if request.method == 'POST' and form.is_submitted():
         try:
-            logger.warning('Inserting cocktail')
             name = form.name.data
+            logger.warning(f'Inserting cocktail {name} in DB')
             description = form.description.data
             ingredients = {f['ingredient_name']: f['quantity'] for f in form.ingredients.data}
             preparation_time_min = form.preparation_time_min.data
             image = form.image.data
             cocktail_type = form.cocktail_type.data
-            cocktail = Cocktail(name=name, description=description, ingredients=ingredients,
+            cocktail = Cocktail(name=name, description=description,
+                                ingredients=ingredients,
                                 preparation_time_min=preparation_time_min,
                                 image=image, cocktail_type=COCKTAIL_NAMES_TYPES[cocktail_type])
 
             res = cocktails_repository.create_one(cocktail)
-        except Exception:
+        except Exception as err:
             logger.warning(f'Error when inserting the cocktail'
-                           f'inserted_id : {res.inserted_id}, cocktail_id : {cocktail._id}')
+                           f'inserted_id : {res.inserted_id}, cocktail_id : {cocktail._id} \n'
+                           f'Error: {err}')
             return render_template('error.html', entity='cocktail')
         return redirect(url_for('home'))
 
