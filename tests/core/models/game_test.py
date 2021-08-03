adc51ea3379670d9a05d871a2ed7851a4cf86415
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-08-03 11:01:43 +0200

    feat(tests) add test for game and cocktail models

---
+++ tests/core/models/game_test.py
@@ -0,0 +1,10 @@
+from core.models.game import *
+
+
+def test_dict_are_complete():
+    for game_type in GameType:
+        assert game_type in GAME_TYPES_NAMES
+        assert game_type in GAME_TYPE_MAPPING.values()
+        assert game_type in GAME_NAMES_TYPES.values()
+        assert game_type in GAME_DEFAULT_IMAGES
+
