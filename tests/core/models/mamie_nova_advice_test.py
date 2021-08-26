538724f567ad87d214d8f8ca84afe02ded84513b
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-08-26 17:25:37 +0200

    feat(mamie-nova-advice) created model for mamie nova advice

---
+++ tests/core/models/mamie_nova_advice_test.py
@@ -0,0 +1,9 @@
+from core.models.mamie_nova_advice import *
+
+
+def test_dict_are_complete():
+    for mamie_nova_advice_type in MamieNovaAdviceType:
+        assert mamie_nova_advice_type in MAMIE_NOVA_TYPES_NAMES
+        assert mamie_nova_advice_type in MAMIE_NOVA_TYPE_MAPPING.values()
+        assert mamie_nova_advice_type in MAMIE_NOVA_NAMES_TYPES.values()
+        assert DEFAULT_IMAGE is not None
